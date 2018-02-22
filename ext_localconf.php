4de11b40d29ddb040077c91b7ad515b1546cf17d
Marc Bastian Heinrichs <typo3@mbh-software.de>
2018-02-22 13:56:39 +0100

    [TASK] Disable this facet type for the moment
    Needs to be migrated to the current solr version

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,13 @@
 <?php
-if (!defined ('TYPO3_MODE')) {
-	die ('Access denied.');
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
 }
 
+/**
 ApacheSolrForTypo3\Solr\Facet\FacetRendererFactory::registerFacetType(
-	'dateRangeBootstrap',
-	'MbhSoftware\\SolrBsDateRangeFacet\\Facet\\DateRangeFacetRenderer',
-	'MbhSoftware\\SolrBsDateRangeFacet\\Query\\FilterEncoder\\DateRange',
-	'MbhSoftware\\SolrBsDateRangeFacet\\Query\\FilterEncoder\\DateRange'
+    'dateRangeBootstrap',
+    'MbhSoftware\\SolrBsDateRangeFacet\\Facet\\DateRangeFacetRenderer',
+    'MbhSoftware\\SolrBsDateRangeFacet\\Query\\FilterEncoder\\DateRange',
+    'MbhSoftware\\SolrBsDateRangeFacet\\Query\\FilterEncoder\\DateRange'
 );
+**/
